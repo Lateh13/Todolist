707fbffc6831e5b43501222fc01cd1367ea99c14
Lauri <hakulinen.lauri@gmail.com>
2021-11-09 15:29:52 +0000

    Changed App.js to only return

--- src/App.js
+++ src/App.js
@@ -1,15 +1,7 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import './App.css'
 import Todolist from "./components/Todolist";
 
 export default function App() {
-    return (
-        ReactDOM.render(
-            <React.StrictMode>
-                <Todolist />
-            </React.StrictMode>,
-            document.getElementById('root')
-        )
-    )
+    return (<Todolist />)
 }
