a9bdacde3a6757609825c6731c05cd56c0aec900
Lauri <hakulinen.lauri@gmail.com>
2021-11-09 13:36:07 +0000

    Separated Todolist.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Todolist from './App';
 import reportWebVitals from './reportWebVitals';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Todolist />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
